5685ad3792a267ea9478600189dfc8280f3b932c
hamza-adeel-brainx <hamza.adeel@brainxtech.com>
2023-05-17 19:22:33 +0500

    ageVerficationFeedbackChangesDone

--- verfication.js
+++ verfication.js
@@ -2,36 +2,51 @@ const day = document.getElementById("day");
 const month = document.getElementById("month");
 const year = document.getElementById("year");
 const btn = document.getElementById("btn");
-btn.addEventListener("click", verifyAge)
+btn.addEventListener("click", validateInput)
+
+function validateInput() {
+    if (day.value == "" || month.value == "" || year.value.length == "") {
+        alert(" fill all fields ");
+    } else if (day.value < 1 || day.value > 31 || month.value < 1 || month.value > 12 || year.value < 1970 || year.value > 2023) {
+        alert("Invalid DOB");
+    } else if (month.value == 2) {
+        if (Number(day.value) > 29) {
+            alert("Invalid DOB");
+        } else if (Number(day.value) == 29) {
+            if (Number(year.value) % 4 != 0) {
+                alert("Invalid DOB");
+            } else {
+                verifyAge();
+            }
+        } else {
+            verifyAge();
+        }
+    } else {
+        verifyAge();
+    }
+}
 
 function verifyAge() {
     const today = new Date;
-    const dob = new Date(year.value,month.value,day.value);
-
-    if (day.value == "" || month.value == "" || year.value.length == "") {
-        alert(" fill all fields ");
-   
-    }
-
-    else if (day.value < 1 || day.value > 31 || month.value < 1 || month.value > 12 || year.value < 1970 || year.value > 2023) {
-        alert("Invalid DOB");
+    const dob = new Date(year.value, month.value, day.value);
+    let yearsDiff = today.getFullYear() - Number(year.value)
+    if (yearsDiff < 15) {
+        alert("age restricted");
+    } else if (yearsDiff == 15) {
+        let monthValue = Number(month.value);
+        let currMonth = today.getMonth() + 1;
+        if (monthValue < currMonth) {
+            alert("age restricted");
+        } else if (monthValue == currMonth) {
+            let dayValue = Number(day.value);
+            let currDay = today.getDate();
+            if (dayValue < currDay) {
+                alert("age restricted");
+            } else {
+                window.location.replace("https://www.google.com/");
+            }
+        }
     } else {
-        var monthDiff = today- dob.getTime();  
-        var age_dt = new Date(monthDiff);   
-        // console.log(age_dt,"age-dt")
-        var year_dt = age_dt.getUTCFullYear();  
-        // console.log(year_dt,"year_dt-dt")
-        var age = Math.abs(year_dt - 1970);  
-        console.log(age)
-         if(age < 15)
-         {
-             alert("Your age is restricted");
-                 
-         }
-         else{
- 
-             window.location.replace("https://www.google.com/");
-         }
+        window.location.replace("https://www.google.com/");
     }
-
 }
